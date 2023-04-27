40ce6a2f2bbed63c8bbbc462204b38153af24102
Julien Huang <julien.huang@outlook.fr>
2023-04-27 22:42:19 +0200

    style: lint client (#8)

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <NCard>
-    <div class="flex items-baseline text-align-bottom"> 
+    <div class="flex items-baseline text-align-bottom">
       <Icon
         name="material-symbols:water-drop-rounded"
         class="text-2xl"
@@ -9,7 +9,7 @@
         nuxt-hydration
       </h1>
     </div>
-    
+
     <p v-if="!devtools">
       awaiting devtools connection
     </p>
@@ -20,27 +20,24 @@
 </template>
 
 <script setup lang="ts">
-import { useDevtoolsClient, onDevtoolsClientConnected } from "@nuxt/devtools-kit/iframe-client"
-import { ClientFunctions, ServerData, ServerFunctions } from "~/../src/types"
-import { RPC_NAMESPACE } from "~/../src/runtime/utils"
-import { ref } from "#imports"
+import { useDevtoolsClient, onDevtoolsClientConnected } from '@nuxt/devtools-kit/iframe-client'
+import { ClientFunctions, ServerData, ServerFunctions } from '~/../src/types'
+import { RPC_NAMESPACE } from '~/../src/runtime/utils'
+import { ref } from '#imports'
 
-const devtools = useDevtoolsClient() 
+const devtools = useDevtoolsClient()
 const serverData = ref<ServerData>({
   routes: {}
 })
 
 onDevtoolsClientConnected(async (client) => {
- 
-  const rpc =  client.devtools.extendClientRpc<ServerFunctions, ClientFunctions>(RPC_NAMESPACE, {
-    updateData(data) {
+  const rpc = client.devtools.extendClientRpc<ServerFunctions, ClientFunctions>(RPC_NAMESPACE, {
+    updateData (data) {
       serverData.value = data
-    },
+    }
   })
 
-
-  serverData.value = await  rpc.getStats()
- 
+  serverData.value = await rpc.getStats()
 })
 
 </script>
