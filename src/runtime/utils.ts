31919d16069fe5b55ee8b12ffb715ea247f83a93
Julien Huang <julien.h.dev@gmail.com>
2023-09-17 11:10:05 +0200

    fix: add addtionnal strings and rely on `nuxt-hydration:component-hydration` for hydration failed state (#72)

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -1,5 +1,6 @@
 export const hydrationMessages = [
-  'Hydration completed but contains mismatches.'
+  'Hydration completed but contains mismatches.',
+  'Hydration text mismatch:'
 ]
 
 export const RPC_NAMESPACE = 'hydration-check-rpc'
